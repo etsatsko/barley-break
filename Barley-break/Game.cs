cd44c5e3ec10ff0429dad1ec56bc01b68578e00a
Elizaveta Trushnikova <etsatsko@mail.ru>
2019-05-02 12:25:23 +0500

    Added buttons, menu, created map, start method

---
+++ Barley-break/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barley_break
+{
+    class Game
+    {
+        int size = 4;
+        int[,] map;
+        int spaceX;
+        int spaceY;
+
+        public Game()
+        {
+            map = new int[size, size];
+        }
+
+        public void Start ()
+        {
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    map[x, y] = ConvertCoordsToRosition(x, y) + 1;
+            spaceX = size - 1;
+            spaceY = size - 1;
+            map[spaceX, spaceY] = 0;
+        }
+
+        public int GetNumber(int position)
+        {
+            int x, y;
+            ConvertPositionToCoords(position, out x, out y);
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+            return map[x, y];
+        }
+
+        private int ConvertCoordsToRosition (int x, int y)
+        {
+            return y * size + x;
+        }
+
+        private void ConvertPositionToCoords(int position, out int x, out int y)
+        {
+            x = position % size;
+            y = position / size;
+        }
+    }
+}
--- Barley-break/MainWindow.xaml.cs
+++ Barley-break/MainWindow.xaml.cs
@@ -15,14 +15,61 @@ using System.Windows.Shapes;
 
 namespace Barley_break
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Game game;
+
         public MainWindow()
         {
             InitializeComponent();
+            game = new Game();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            game.Start();
+            RefreshButton();
+        }
+
+        private void RefreshButton()
+        {
+            for (int position = 0; position < 16; position++)
+                button(position).Content = game.GetNumber(position).ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            button(position).Content = position.ToString();
+        }
+
+        private Button button(int position)
+        {
+            switch(position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
         }
     }
 }
